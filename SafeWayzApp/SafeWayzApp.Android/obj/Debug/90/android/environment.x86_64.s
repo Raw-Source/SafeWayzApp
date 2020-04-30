524e11313af19d7880a2cd4518d87b21b4f31ec2
Abdul-khaaliq Dollie <55828788+Abdulkhaaliq@users.noreply.github.com>
2020-04-30 09:52:59 +0200

    Added

--- SafeWayzApp/SafeWayzApp.Android/obj/Debug/90/android/environment.x86_64.s
+++ SafeWayzApp/SafeWayzApp.Android/obj/Debug/90/android/environment.x86_64.s
@@ -69,7 +69,7 @@ mono_aot_mode_name:
 	.section	.rodata..L.str.8,"aMS",@progbits,1
 	.type	.L.str.8, @object
 .L.str.8:
-	.asciz	"bb59b992-a0f3-4862-b936-50b157ecb7ae"
+	.asciz	"bdc9d085-6b58-458a-8b10-1ac3aeb19898"
 	.size	.L.str.8, 37
 	.section	.rodata..L.str.9,"aMS",@progbits,1
 	.type	.L.str.9, @object
