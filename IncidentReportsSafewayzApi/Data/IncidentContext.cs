8b08eb759803d81de47c8372e7fb203b84a84672
Abdul-khaaliq Dollie <55828788+Abdulkhaaliq@users.noreply.github.com>
2020-05-18 18:39:35 +0200

    Added

---
+++ IncidentReportsSafewayzApi/Data/IncidentContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SafeWayzApp.Models;
+using SafeWayzLibrary.Models;
+
+namespace IncidentReportsSafewayzApi.Data
+{
+    public class IncidentContext : DbContext
+    {
+        public IncidentContext(DbContextOptions<IncidentContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<IncidentReportModel> IncidentReports { get; set; }
+    }
+}
---
+++ IncidentReportsSafewayzApi/Data/SeedData.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SafeWayzLibrary.Models;
+
+namespace IncidentReportsSafewayzApi.Data
+{
+    public class SeedData
+    {
+        public static void Initialize(IncidentContext context)
+        {
+            if (!context.IncidentReports.Any())
+            {
+                context.IncidentReports.AddRange(
+                    new IncidentReportModel
+                    {
+                      
+                    }
+                );
+
+                context.SaveChanges();
+            }
+        }
+    }
+}
